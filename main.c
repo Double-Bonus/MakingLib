0889cb7d934a4cbc08f723c26a12e5ad0384bfb5
Zygimantas <zygimantasmarma@gmail.com>
2021-06-19 23:19:41 +0300

    Added library

--- main.c
+++ main.c
@@ -15,5 +15,7 @@ int main(int argc, char* argv[])
             printf("\nargv[%d]: %s", counter, argv[counter]);
     }
 
+    greatStudent();
+
     return 0;
 }
---
+++ uniLib/src/uniLib.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "uniLib.h"
+
+void greatStudent()
+{
+    printf("Welcome to university!!!\n");
+}
